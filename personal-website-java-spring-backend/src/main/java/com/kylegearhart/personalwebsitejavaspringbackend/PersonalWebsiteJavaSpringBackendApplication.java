c8c7d3ad280b08ca523f3c0ab47bb449e38500a5
kylegearhart <hkgearhart@gmail.com>
2018-10-13 18:26:34 +0900

    initial commit

---
+++ personal-website-java-spring-backend/src/main/java/com/kylegearhart/personalwebsitejavaspringbackend/PersonalWebsiteJavaSpringBackendApplication.java
@@ -0,0 +1,12 @@
+package com.kylegearhart.personalwebsitejavaspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteJavaSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteJavaSpringBackendApplication.class, args);
+	}
+}
---
+++ personal-website-java-spring-backend/src/test/java/com/kylegearhart/personalwebsitejavaspringbackend/PersonalWebsiteJavaSpringBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kylegearhart.personalwebsitejavaspringbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalWebsiteJavaSpringBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
