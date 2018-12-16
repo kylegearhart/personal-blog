9da18baef041ffd52a8cc84d990176468b8807c2
kylegearhart <hkgearhart@gmail.com>
2018-12-16 19:28:35 +0900

    add About content

--- frontend/src/app/about/about.component.spec.ts
+++ frontend/src/app/about/about.component.spec.ts
@@ -1,4 +1,23 @@
 import { AboutComponent } from './about.component'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 describe('AboutComponent', () => {
+  let fixture: ComponentFixture<AboutComponent>
+  let subjectHTMLElement: HTMLElement
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AboutComponent ],
+    })
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent)
+    subjectHTMLElement = fixture.nativeElement
+  })
+
+  it('displays my About blurb', () => {
+    const aboutElement: HTMLElement = subjectHTMLElement.querySelector('.about')
+    expect(aboutElement.innerText).toContain('Kyle Gearhart')
+  })
 });
--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core'
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
+  aboutBlurb = `
+    My name is Kyle Gearhart, a software engineer working at Pivotal Labs in Tokyo.
+    I'm in love with languages—those of the computer, human, and musical variety all pique
+    my interest greatly. Until now, I've spent much time learning how things are expressed
+    with these mediums, and now I'm turning to expressing myself through them. I hope to
+    contribute to a number of subjects, but the most important to me are software craftsmanship,
+    language learning, nutrition, and ecological sustainability.
+   `
 }
