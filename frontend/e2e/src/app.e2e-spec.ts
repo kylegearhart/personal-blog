8aad9689922f3e0e07506b2abc46acfc01fb7c86
kylegearhart <hkgearhart@gmail.com>
2019-03-21 09:29:16 +0900

    minor formatting changes to e2e test

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { BlogHomePage } from './app.po'
 
 describe('personal website', () => {
-  const blogHomePage = new BlogHomePage();
+  const blogHomePage = new BlogHomePage()
 
   beforeEach(async () => {
     await blogHomePage.navigateTo()
   });
 
   it('should display blog title', async () => {
-    expect(await blogHomePage.getTitleText())
-      .toEqual('Hello World');
+    expect(await blogHomePage.getTitleText()).toEqual('Hello World')
   });
 
   it('should display blog article titles', async () => {
