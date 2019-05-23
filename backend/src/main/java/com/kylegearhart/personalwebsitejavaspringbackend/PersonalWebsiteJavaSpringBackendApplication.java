2f9a49cdb42e677fc8e324a9f4d3cbee4a0003e8
kylegearhart <hkgearhart@gmail.com>
2019-05-23 14:14:17 -0400

    fix reload failures by adding a redirect to index.html when users encounter a 404

--- backend/src/main/java/com/kylegearhart/personalwebsitejavaspringbackend/PersonalWebsiteJavaSpringBackendApplication.java
+++ backend/src/main/java/com/kylegearhart/personalwebsitejavaspringbackend/PersonalWebsiteJavaSpringBackendApplication.java
@@ -2,11 +2,25 @@ package com.kylegearhart.personalwebsitejavaspringbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class PersonalWebsiteJavaSpringBackendApplication {
+public class PersonalWebsiteJavaSpringBackendApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalWebsiteJavaSpringBackendApplication.class, args);
+	}
+
+	@RequestMapping("/error")
+	public String forwardUserOntoAngularApp() {
+		return "forward:/index.html";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 }
