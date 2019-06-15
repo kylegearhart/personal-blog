738f6b3d39b02367a794c46a05570ca59fc05697
kylegearhart <hkgearhart@gmail.com>
2019-06-15 13:33:30 +0900

    style everything to look DECENT!

--- frontend/src/app/blog/blog-article-detail/blog-article-detail.component.ts
+++ frontend/src/app/blog/blog-article-detail/blog-article-detail.component.ts
@@ -5,7 +5,7 @@ import { BlogArticleDetails } from '../model-objects'
 @Component({
   selector: 'app-blog-article-detail',
   templateUrl: './blog-article-detail.component.html',
-  styleUrls: ['./blog-article-detail.component.css'],
+  styleUrls: ['./blog-article-detail.component.scss'],
 })
 export class BlogArticleDetailComponent implements OnInit {
   articleDetails: BlogArticleDetails = new BlogArticleDetails('', '')
